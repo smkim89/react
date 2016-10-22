91b604127926828f595823533cd91a762f4e2537
smkim89 <kiak357@gmail.com>
2016-10-22 13:34:27 +0900

    컴포턴트 props 추가했음~

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App headerTitle = "Welcome!"
-                     contentTitle = "Stranger,"
-                     contentBody = "Welcome to example app"/>, rootElement);
+
+App.defaultProps = {
+    headerTitle: '헤더',
+    contentTitle: '내용 타이틀',
+    contentBody: '내용 바디'
+};
+
+ReactDOM.render(<App />, rootElement);
